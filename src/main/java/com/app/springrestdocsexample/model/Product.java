f224a80cb9b1bfd37c3dd4e528d21d0ec06b180a
pedrohubner <pedronhubner@gmail.com>
2021-11-26 14:28:48 -0300

    Creating model.

---
+++ src/main/java/com/app/springrestdocsexample/model/Product.java
@@ -0,0 +1,30 @@
+package com.app.springrestdocsexample.model;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", value=" + price +
+                '}';
+    }
+}
