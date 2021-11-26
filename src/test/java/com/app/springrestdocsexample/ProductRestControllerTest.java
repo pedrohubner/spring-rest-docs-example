6a4cb1cedaaaf29db5d4241ed5480d97c3bff81f
pedrohubner <pedronhubner@gmail.com>
2021-11-26 14:28:34 -0300

    Refactoring controller test and documentation.

--- src/test/java/com/app/springrestdocsexample/ProductRestControllerTest.java
+++ src/test/java/com/app/springrestdocsexample/ProductRestControllerTest.java
@@ -1,5 +1,7 @@
 package com.app.springrestdocsexample;
 
+import com.app.springrestdocsexample.model.Product;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,31 +11,32 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = HomeController.class)
+@WebMvcTest(controllers = ProductRestController.class)
 @ExtendWith({RestDocumentationExtension.class})
 @AutoConfigureRestDocs(outputDir = "build/snippets")
-class HomeControllerTest {
+class ProductRestControllerTest {
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper mapper;
 
     @Test
     void shouldGetGreeting() throws Exception {
-        mockMvc.perform(get("/")
+        Product product = new Product("Dorflex", 4.9);
+        mockMvc.perform(get("/v1/product")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, World")))
-                .andDo(document("home",
-                        responseFields(fieldWithPath("message").description("The welcome message for the user"))));
+                .andExpect(content().json(mapper.writeValueAsString(product)))
+                .andDo(document("{class-name}/{method-name}", preprocessResponse(prettyPrint())));
     }
 }
